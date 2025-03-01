a464ecbf5f69d503bffda0379634e66209b5aec1
therealsamyak <therealsamyak@gmail.com>
2025-02-28 21:48:51 -0800

    added sgd and its functionality

---
+++ sgd.py
@@ -0,0 +1,59 @@
+import numpy as np
+from data_tokenization import DataPoint, read_and_process_file
+from logistic_regression import binary_cross_entropy_loss, classifier
+
+
+def sgd(train_data: list[DataPoint], learning_rate: float, epochs: int = 100):
+    w = np.zeros(3)
+    b = 0
+
+    for _ in range(epochs):
+        np.random.shuffle(train_data)
+        for data_point in train_data:
+            y_true = data_point.label
+            y_pred = classifier(data_point, w=w, b=b)
+
+            error = y_pred - y_true
+            w -= learning_rate * error * np.array(data_point.features)
+            b -= learning_rate * error
+
+    return w, b
+
+
+if __name__ == "__main__":
+    learning_rates: list[float] = [
+        0.00001,
+        0.00005,
+        0.0001,
+        0.0005,
+        0.001,
+        0.005,
+        0.01,
+        0.05,
+        0.1,
+        0.5,
+    ]
+
+    best_lr = None
+    lowest_val_loss = float("inf")
+    results = {}
+
+    train_data, val_data, _ = read_and_process_file()
+    for lr in learning_rates:
+        w, b = sgd(train_data, lr)
+        val_loss = np.mean(
+            [binary_cross_entropy_loss(data_pt, w, b) for data_pt in val_data]
+        )
+        results[lr] = val_loss
+
+        if val_loss < lowest_val_loss:
+            lowest_val_loss = val_loss
+            best_lr = lr
+
+    print()
+    print(f"Best Learning Rate: {best_lr}")
+    print(f"Lowest Validation Loss: {lowest_val_loss}")
+    print()
+    print("Validation Loss for Each Learning Rate:")
+    for lr, loss in results.items():
+        print(f"Learning Rate: {lr}, Loss: {loss}")
